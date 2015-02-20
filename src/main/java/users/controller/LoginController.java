be268d440e2dc8d14195c449a338b51af4450a97
Tomas Shestakov <shestakovte@gmail.com>
2015-02-20 21:26:26 +0300

    user details template

--- src/main/java/users/controller/LoginController.java
+++ src/main/java/users/controller/LoginController.java
@@ -2,17 +2,31 @@ package users.controller;
 
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import users.security.CurrentUser;
+
+import java.util.Map;
 
 @Controller
 public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(@AuthenticationPrincipal CurrentUser currentUser) {
+    public String login(@AuthenticationPrincipal CurrentUser currentUser, @RequestParam Map<String, String> params, Model model) {
         if (currentUser != null) {
             return "redirect:/";
+        }
+
+        if (params.containsKey("error")) {
+            model.addAttribute("error", true);
+        }
+        else if (params.containsKey("logout")) {
+            model.addAttribute("logout", true);
+        }
+        else {
+            model.addAttribute("login", true);
         }
 
         return "login";
--- src/main/java/users/controller/UserController.java
+++ src/main/java/users/controller/UserController.java
@@ -3,21 +3,25 @@ package users.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import users.repository.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    final Sort USERS_SORT = new Sort("email");
 
     @Autowired
     UserRepository userRepository;
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public ModelAndView list() {
-        return new ModelAndView("users", "users", userRepository.findAll(new Sort("email")));
+        return new ModelAndView("users", "users", userRepository.findAll(USERS_SORT));
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
@@ -25,8 +29,16 @@ public class UserController {
         return "user";
     }
 
-    @RequestMapping(value = "/update", method = RequestMethod.GET)
-    public String update() {
+    @RequestMapping(value = "/update{id}", method = RequestMethod.GET)
+    public String update(@PathVariable("id") long id) {
         return "user";
     }
+
+    @RequestMapping(value = "/list1", method = RequestMethod.GET)
+    public RedirectView redirectList(RedirectAttributes redirectAttributes) {
+        final RedirectView redirectView = new RedirectView("/user/list");
+        redirectView.setPropagateQueryParams(true);
+        return redirectView;
+    }
+
 }
